d150fcc9255649c7700d2dab13f1c7f9d582bc26
Dimitar <xektoro@abv.bg>
2019-12-29 15:27:06 +0200

    Updated IndexController Unit test with MockMvc

--- src/test/java/com/example/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/example/spring5recipeapp/controllers/IndexControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -15,7 +17,12 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+// when we are testing controllers, we can do just unit tests (there in no need to bring the Spring Context, because
+// it is making our test very slow)
 class IndexControllerTest {
     // here we need Mocks for our Service and Model data
     @Mock
@@ -30,6 +37,19 @@ class IndexControllerTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
+    }
+
+    // here we will introduce a mock web server (or mock dispatcher servlet)
+    @Test
+    void testMockMvc() throws Exception {
+        // if we specify MockMvcBuilders.webAppContextSetup() - we are going to bring Spring Context and
+        // our tests will no longer be a unit tests.
+        // but MockMvcBuilders.standaloneSetup() will keep our tests running nice and fast (not bringing Spring Context)
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
